08fbdebd60a977e29528409cd4205324c258fcf6
L0uzzDev <l0uzzdev@gmail.com>
2024-08-06 19:47:06 -0300

    Front-end Financer

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FormEmp from "./components/FormEmp";
+import "../src/styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <FormEmp />
+  </React.StrictMode>
+);
